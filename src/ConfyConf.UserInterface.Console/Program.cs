6127806280d980a80cc506cdb116f988555f1382
tugberkugurlu <tugberkugurlu@gmail.com>
2015-03-28 20:44:02 +0000

    waiting on the console

--- src/ConfyConf.UserInterface.Console/Program.cs
+++ src/ConfyConf.UserInterface.Console/Program.cs
@@ -4,6 +4,8 @@ using ConfyConf.Commands;
 
 namespace ConfyConf.UserInterface.Console
 {
+    using Console = System.Console;
+
     class Program
     {
         static void Main(string[] args)
@@ -13,6 +15,8 @@ namespace ConfyConf.UserInterface.Console
             ICommandDispatcher dispatcher = Bootstraper.Initialize();
             CreateUserCommand createUserCmd = new CreateUserCommand(NewId, "Tugberk");
             dispatcher.Dispatch(createUserCmd);
+
+            Console.ReadLine();
         }
 
         static string NewId
